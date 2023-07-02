d12fe83d6bd12cdbf76148eaeb3a0e0f5d30af12
tapu18 <118423392+tapu18@users.noreply.github.com>
2023-07-02 15:04:11 +0900

    細かい修正

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 import { world, system, MinecraftBlockTypes} from "@minecraft/server"
 import * as editor from "@minecraft/server-editor";
-import * as smoother from "./modules/smoother";
-import * as blendFill from "./modules/blend_fill";
-import * as perlin from "./modules/perlin";
-import * as sphere from "./modules/sphere";
-import * as cylinder from "./modules/cylinder"
+import * as smoother from "./extensions/smoother";
+import * as blendFill from "./extensions/blend_fill";
+import * as perlin from "./extensions/perlin";
+import * as sphere from "./extensions/sphere";
+import * as cylinder from "./extensions/cylinder"
 
 sphere.default();
+cylinder.default();
 smoother.default();
 blendFill.default();
 perlin.default();
-cylinder.default();
